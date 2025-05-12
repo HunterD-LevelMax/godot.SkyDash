da6703fa2422248d3c2d341e52e98bc49c1b1620
Hunter <warsvitaly@gmail.com>
2025-05-12 18:38:28 +0800

    init

--- Assets/Platform/win_platform.gd
+++ Assets/Platform/win_platform.gd
@@ -4,4 +4,5 @@ signal platform_win
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
-		emit_signal("platform_win") # Сигнал о победе
+		#emit_signal("platform_win") # Сигнал о победе
+		pass
